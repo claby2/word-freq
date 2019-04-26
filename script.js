505069963cddc2ba434e069dbed2a2a1a62720e5
Edward Wibowo <43205692+claby2@users.noreply.github.com>
2019-04-26 18:17:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+function pushWords(arr){
+    list.childNodes.forEach(e=>setTimeout(()=>list.removeChild(e), 1));
+    arr.forEach(e=>{
+        let tr = document.createElement("tr");
+        let tdword = document.createElement("td");
+        let tdfreq = document.createElement("td");
+        tdword.innerText = e[0];
+        tdfreq.innerText = e[1];
+        tr.appendChild(tdword);
+        tr.appendChild(tdfreq);
+        list.appendChild(tr);
+    })
+}
+
+words = [];
+
 let submit = document.getElementById("submit-id");
 let list = document.getElementById("list");
 let inputel = document.getElementById("input-id");
 
 submit.addEventListener("click", ()=>{
-    let p = new Promise((resolve, reject)=>{
-        list.childNodes.forEach(e=>setTimeout(()=>list.removeChild(e), 1));
-        resolve(!list.childNodes);
-    }).then(undef=>{
         let input = inputel.value;
         let freqMap = {};
         let arr = input.replace(/[^\w\s]/g,"").split(/\s+/g).filter(e=>!!e)
             .map(e=>e.toLowerCase())
             .forEach(e=>freqMap[e] ? freqMap[e]++ : freqMap[e] = 1);
-        Object.entries(freqMap).sort((a,b) => b[1] - a[1]).forEach(e=>{
-            let el = document.createElement("li");
-            el.innerText = e.join(' ');
-            list.appendChild(el);
-        })
-    })
+        words = Object.entries(freqMap).sort((a,b) => b[1] - a[1]);
+        pushWords(words);
 });
+
+let search = document.getElementById("search-id");
+
+search.addEventListener("keyup", ()=>{
+    let target = search.value;
+    filtered = words.filter((e)=>
+        e[0].indexOf(target) != -1
+    );
+    pushWords(filtered);
+
+})
